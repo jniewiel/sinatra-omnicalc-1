0852fef7e207803b01a29a8bd1ab9a7608882f8c
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-03-18 18:04:09 +0000

    Completed payment and random views

--- app.rb
+++ app.rb
@@ -34,7 +34,7 @@ end
 # ----------------------------------------- #
 
 get("/square/results") do
-  @number = params.fetch("number").to_i
+  @number = params.fetch("number").to_f
   @square = (@number * @number).to_f
 
   erb(:square_results)
@@ -43,7 +43,7 @@ end
 # ----------------------------------------- #
 
 get("/square_root/results") do
-  @user_number = params.fetch("user_number").to_i
+  @user_number = params.fetch("user_number").to_f
   @square_root = Math.sqrt(@user_number).to_f
   
   erb(:square_root_results)
@@ -52,12 +52,26 @@ end
 # ----------------------------------------- #
 
 get("/payment/results") do
+  @user_apr = (params.fetch("user_apr").to_f)
+  @user_years = params.fetch("user_years").to_i
+  @user_pv = params.fetch("user_pv").to_i
+
+  apr = ((@user_apr / 100)/ 12)
+  years = @user_years * 12
+  @payment = (@user_pv * apr * (1 + apr) ** years / ((1 + apr) ** years - 1)).to_fs(:currency, {:precision => 2})
+
+  @apr_print = @user_apr.to_fs(:percentage, {:precision => 4})
+  @principal_print = @user_pv.to_fs(:currency, {:precision => 2})
   erb(:payment_results)
 end
 
 # ----------------------------------------- #
 
 get("/random/results") do
+  @minimum = params.fetch("user_min").to_i
+  @maximum = params.fetch("user_max").to_i
+  @random_number = rand(@minimum..@maximum).to_f
+
   erb(:random_results)
 end
 
